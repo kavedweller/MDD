ed4cec4e285a574c980b3503618211d9633e0b22
Anisul Kabir Chowdhury <kbrc.net@gmail.com>
2019-04-18 12:45:20 +0600

    Update MDD.cmd

--- MDD.cmd
+++ MDD.cmd
@@ -1,7 +1,7 @@
 @ECHO OFF
 REM Make Dated Directory
 REM Creates a subdirectory in the present working directory
-REM with current date as name (as YYYY-MM-DD) in Windows Vista/7/10 
+REM with current date as name (as YYYY-MM-DD) in Windows Vista/7/8.1/10 
 
 FOR /F "usebackq tokens=1,2 delims==" %%i in (`wmic os get LocalDateTime /VALUE 2^>NUL`) do if '.%%i.'=='.LocalDateTime.' set ldt=%%j
 REM SET ldt=%ldt:~0,4%-%ldt:~4,2%-%ldt:~6,2% %ldt:~8,2%:%ldt:~10,2%:%ldt:~12,6%
